5c985adc7b1f94de426fd3e3006629107f3f7292
Julia Alberto <julia.elyssa.alberto@gmail.com>
2022-07-06 12:22:50 -0700

    Start of new react app

---
+++ julia-alberto/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import * as React from "react";
+import { Router, Routes, Route,} from "react-router-dom";
+import './App.css';
+
+import Home from './pages/Home/home';
+import About from './pages/About/about';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Router>
+
+
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <Routes>
+        <Route path = "/" element={<Home/>}/>
+        <Route path = "/about" element={<About/>}/> 
+      </Routes>
+      </Router>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
